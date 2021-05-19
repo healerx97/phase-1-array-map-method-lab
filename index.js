a90fa150396e9afdfeb89dd5b9ec58e8629b89a9
Kenny Yoon <ghun.yoon@gmail.com>
2021-05-19 02:43:36 -0400

    completed

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
+let n = 0
+const newTutorials = []
+function fn(sent) {
+  let newSent = ""
+  for (let i in sent) {
+    if (sent[i] == sent[i].toLowerCase() && sent[i-1] == " ") {
+      newSent+=(sent[i].toUpperCase())
+    } else {
+      newSent+=(sent[i])
+    }
+  }
+  return newSent[0].toUpperCase() + newSent.slice(1)
+}
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(fn)
 }
